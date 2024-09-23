baf33ff0129dc9ce9203347bc6650acc83ab1d3b
EhevaNik <eheva@yandex.ru>
2024-09-23 21:31:07 +0500

    Update module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -2,5 +2,5 @@ def test_function():
     def inner_function():
         print("Я в области видимости функции test_function")
     inner_function()
-
+# Вызов inner_function вне функции test_function выдает ошибку при результате выполнения программы
 test_function()
